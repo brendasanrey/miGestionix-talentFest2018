318950f6dde91e6386a8a952ac1970efb43db96e
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-12-03 14:54:36 -0600

    subiendo componente inicial

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <input type="email" placeholder="username"> |
+    <input type="password" placeholder="password">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+};
+</script>
