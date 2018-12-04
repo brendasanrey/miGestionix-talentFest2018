f8aae9c5bb365a07797f75779d49818c573b805e
flor <l.tgessner@gmail.com>
2018-12-04 10:41:44 -0600

    vue-speech added

--- src/components/AccountsPending.vue
+++ src/components/AccountsPending.vue
@@ -1,80 +1,22 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">MiGestionix</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="form-inline my-2 my-lg-0 ml-auto">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0 mr-2" type="submit">Buscar</button>
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cerrar Sesión</button>
-        </form>
-      </div>
-    </nav>
-    <div class="container-fluid mt-5">
-      <div class="row">
-        <div class="col-md-8 offset-md-2">
-          <div class="table-responsive">
-            <table class="table">
-              <thead class="text-center">
-                <tr>
-                  <th scope="col">Compañía</th>
-                  <th scope="col">Cantidad</th>
-                </tr>
-              </thead>
-              <tbody class="text-center" v-for="client in clients" :key="client.id">
-                <tr v-for="account in client.accounts_pending" v-if="account.total>0" :key="account.id">
-                  <td>{{client.business_name}}</td>
-                  <td>
-                    <ul class="list-group list-group-flush text-center">
-                      <li class="list-group-item text-center">{{account.total}}</li>
-                    </ul>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<form><input
+                type="text"><vue-speech lang="es-ES" @onTranscriptionEnd="onEnd" /></form>
+               
 </template>
 
 
 <script>
-import { mapGetters } from "vuex";
-
 export default {
-  name: "accountsPending",
-  computed: {
-    ...mapGetters(["clients"])
-  },
-  created() {
-    if (!localStorage.getItem("access_token")) {
-      this.$router.push("/");
-    } else {
-      this.getListOfClients();
-    }
-  },
   methods: {
-    getListOfClients() {
-      this.$store.dispatch("getList");
+    onEnd ({ lastSentence, transcription }) {
+      // `lastSentence` is the last sentence before the pause
+      // `transcription` is the full array of sentences
     }
   }
-};
+}
 </script>
 
-<style scoped>
-.table-responsive {
-  background-color: #fff;
-}
-
-thead {
-  background-color: #e98823;
-}
+<style scoped lang="css">
 </style>
 
 
