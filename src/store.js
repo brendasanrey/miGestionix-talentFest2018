d5568de52146b4ce62c58b95bfa5e71975ffe96b
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-12-03 18:07:50 -0600

    agregando login

--- src/store.js
+++ src/store.js
@@ -1,16 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
+  state: {},
+  mutations: {},
   actions: {
-
+    signinUser: ({ commit }, payload) => {
+      console.log(payload);
+    }
   }
-})
+});
